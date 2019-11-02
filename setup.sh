3180c48cc0f5e6eab9fc34523bdc901f625d4c88
Nageen Chand <25462010+archie9211@users.noreply.github.com>
2019-11-02 17:04:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@ apt-select
 cp sources.list /etc/apt/sources.list
 apt update
 apt install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/akhilnarang/scripts
-cd scripts
-bash setup/android_build_env.sh
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k\
-git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp .oh-my-zsh  ~/.oh-my-zsh -r
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# git clone https://github.com/akhilnarang/scripts
+# cd scripts
+# bash setup/android_build_env.sh
+cp .zshrc ~/.zshrc
+# git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k\
+# git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
